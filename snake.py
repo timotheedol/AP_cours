47ce6c3c768ae5b3540963d6b4d021b6599bf17f
Timothée Dol <timothee.dol@etu.minesparis.psl.eu>
2022-11-01 18:23:44 +0100

    black sur le fichier snake.py

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+from curses import KEY_LEFT
 import random
 import sys
 import pygame
@@ -23,44 +24,41 @@ while True:
             if event.key == pygame.K_q:
                 pygame.quit()
                 sys.exit()
-        
-            elif event.key == pygame.K_RIGHT:
-                
-        
-            elif event.key == pygame.K_LEFT:
-                print('<-')
-        
-            elif event.key == pygame.K_UP:
-                print('haut')
-                
-            elif event.key == pygame.K_DOWN:
-                print('bas')
-    
-    screen.fill([0,0,0])
-    
 
-    def is_white(i,j):
-        if (i+j) % 2==0:
+            elif event.key == pygame.K_RIGHT:
+                print("droite")
+
+            elif event.key == pygame.K_LEFT:
+                print("gauche")
+
+            elif event.key == pygame.K_UP:
+                print("haut")
+
+            elif event.key == pygame.K_DOWN:
+                print("bas")
+
+    screen.fill([0, 0, 0])
+
+    def is_white(i, j):
+        if (i + j) % 2 == 0:
             return True
         else:
             return False
 
     for i in range(30):
         for j in range(30):
-            x = i*20
-            y = j*20
+            x = i * 20
+            y = j * 20
             width = 20
             height = 20
             rect = [x, y, width, height]
-            if is_white(i,j):
-                color = [255,255,255]
+            if is_white(i, j):
+                color = [255, 255, 255]
                 pygame.draw.rect(screen, color, rect)
 
-    for x,y in snake:
-        rect = [20*x,20*y,20,20]
-        pygame.draw.rect(screen, [255,0,0], rect)
+    for x, y in snake:
+        rect = [20 * x, 20 * y, 20, 20]
+        pygame.draw.rect(screen, [255, 0, 0], rect)
 
-           
     pygame.display.update()
     clock.tick(1)
-    
